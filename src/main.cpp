86867aeb0b11a0025b29589293aee933f48218de
Zoltán Nagy <abesto@abesto.net>
2021-11-14 16:21:18 +0000

    Everyone needs a little ECS

--- src/main.cpp
+++ src/main.cpp
@@ -1,80 +1,138 @@
 #include <SDL2/SDL.h>
 #include <libtcod.h>
 
+#include <entt/entt.hpp>
 #include <optional>
 #include <variant>
 
 #include "action.h"
 #include "keymap.h"
 
+struct State {
+  bool quit;
+};
+
+struct Player {};
+
+struct Renderable {
+  char glyph;
+};
+
+void render(entt::registry& registry) {
+  auto& console = registry.ctx<tcod::Console>();
+  auto& context = registry.ctx<tcod::ContextPtr>();
+
+  console.clear();
+  for (auto&& [entity, renderable, position] : registry.view<Renderable, Coord>().each()) {
+    console.at(position.x, position.y).ch = renderable.glyph;
+  }
+  context->present(console);
+}
+
+void wait_for_input() { SDL_WaitEvent(nullptr); }
+
+bool input(entt::registry& registry) {
+  // Wait for input
+  SDL_Event event;
+  if (SDL_PollEvent(&event)) {
+    registry.ctx<tcod::ContextPtr>()->convert_event_coordinates(event);
+    registry.set<const SDL_Event>(event);
+    return true;
+  } else {
+    registry.unset<SDL_Event>();
+    return false;
+  }
+}
+
+void handle_quit_request(entt::registry& registry) {
+  if (auto* event = registry.try_ctx<SDL_Event>()) {
+    if (event->type == SDL_QUIT) {
+      registry.ctx<State>().quit = true;
+    } else if (event->type == SDL_KEYDOWN) {
+      auto keymap = registry.ctx<keymap::Keymap>();
+      if (keymap.is_action(event->key.keysym.sym, InputAction::Quit)) {
+        registry.ctx<State>().quit = true;
+      }
+    }
+  }
+}
+
+void handle_input(entt::registry& registry) {
+  // Translate InputAction to Action
+  std::optional<action::Action> action;
+  if (auto* event = registry.try_ctx<const SDL_Event>(); event != nullptr && event->type == SDL_KEYDOWN) {
+    auto keymap = registry.ctx<keymap::Keymap>();
+    if (auto input_action = keymap.get_action(event->key.keysym.sym); input_action) {
+      if (input_action == InputAction::East) {
+        action = action::Walk{1, 0};
+      } else if (input_action == InputAction::West) {
+        action = action::Walk{-1, 0};
+      } else if (input_action == InputAction::North) {
+        action = action::Walk{0, -1};
+      } else if (input_action == InputAction::South) {
+        action = action::Walk{0, 1};
+      }
+    }
+  }
+
+  // Assign the action to any player characters
+  if (action) {
+    for (auto&& [entity] : registry.view<Player>().each()) {
+      registry.emplace<action::Action>(entity, *action);
+    }
+  }
+
+  // Clean up
+  registry.unset<SDL_Event>();
+}
+
+void walk(entt::registry& registry) {
+  for (auto&& [entity, position, action] : registry.view<Coord, action::Action>().each()) {
+    if (std::holds_alternative<action::Walk>(action)) {
+      auto candidate_position = position + std::get<action::Walk>(action).vector;
+      if (candidate_position.x >= 0 && candidate_position.x < 80 && candidate_position.y >= 0 &&
+          candidate_position.y < 25) {
+        registry.replace<Coord>(entity, candidate_position);
+      }
+      registry.erase<action::Action>(entity);
+    }
+  }
+}
+
 int main(int argc, char* argv[]) {
-  auto console = tcod::Console{80, 25};  // Main console.
+  entt::registry registry;
+  registry.set<tcod::Console>(80, 25);
 
   // Configure the context.
   auto params = TCOD_ContextParams{};
   params.tcod_version = TCOD_COMPILEDVERSION;
-  params.console = console.get();
-  params.window_title = "Libtcod Project";
+  params.console = registry.ctx<tcod::Console>().get();
+  params.window_title = "crl";
   params.sdl_window_flags = SDL_WINDOW_RESIZABLE;
   params.vsync = true;
   params.argc = argc;
   params.argv = argv;
-
   auto context = tcod::new_context(params);
 
-  Coord position{40, 12};
-  auto current_keymap = keymap::main;
-  bool quit = false;
+  registry.set<tcod::ContextPtr&>(context);
 
-  while (!quit) {  // Game loop.
-    // Render current state
-    console.clear();
-    console.at(position.x, position.y).ch = '@';
-    context->present(console);
+  const auto player = registry.create();
+  registry.emplace<Player>(player);
+  registry.emplace<Coord>(player, 40, 12);
+  registry.emplace<Renderable>(player, '@');
 
-    // Wait for input
-    SDL_Event event;
-    SDL_WaitEvent(nullptr);  // Optional, sleep until events are available.
+  registry.set<keymap::Keymap>(keymap::main);
+  registry.set<State>(false);
 
-    // Handle all the events we got
-    while (SDL_PollEvent(&event)) {
-      context->convert_event_coordinates(event);  // Optional, converts pixel coordinates into tile coordinates.
-
-      if (event.type == SDL_QUIT) {
-        quit = true;
-        continue;
-      }
-
-      // input_action = keymap[input]
-      if (event.type == SDL_KEYDOWN) {
-        if (auto it = current_keymap.find(event.key.keysym.sym); it != current_keymap.end()) {
-          auto input_action = it->second;
-
-          // input_action -> action
-          std::optional<action::Action> action;
-          if (input_action == InputAction::East) {
-            action = action::Walk{1, 0};
-          } else if (input_action == InputAction::West) {
-            action = action::Walk{-1, 0};
-          } else if (input_action == InputAction::North) {
-            action = action::Walk{0, -1};
-          } else if (input_action == InputAction::South) {
-            action = action::Walk{0, 1};
-          } else if (input_action == InputAction::Quit) {
-            quit = true;
-          }
-
-          // Handle action: walking
-          if (action != std::nullopt && std::holds_alternative<action::Walk>(*action)) {
-            auto candidate_position = position + std::get<action::Walk>(*action).vector;
-            if (candidate_position.x >= 0 && candidate_position.x < 80 && candidate_position.y >= 0 &&
-                candidate_position.y < 25) {
-              position = candidate_position;
-            }
-          }
-        }
-      }
+  while (!registry.ctx<const State>().quit) {  // Game loop.
+    render(registry);
+    wait_for_input();
+    while (input(registry)) {
+      handle_quit_request(registry);
+      handle_input(registry);
+      walk(registry);
     }
   }
+
   return 0;
 }
